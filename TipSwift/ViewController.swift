c40fa5150b1dd286e12dae4dafe674354b3f39fa
DJ Chung <djchung3@gmail.com>
2014-06-03 17:12:39 -0500

    Initial Commit

---
+++ TipSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipSwift
+//
+//  Created by Dong-Jae Chung on 6/3/14.
+//  Copyright (c) 2014 Dong-Jae Chung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
